f021baadfccaa97d63b4562a0a4110ce248ce702
Fatema <fatematz486@gmail.com>
2026-05-30 20:00:58 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 import Image from "next/image";
 
 import Banner from "@/components/Banner";
+import FeaturesJob from "@/components/FeaturesJob";
+import Pricing from "@/components/Pricing";
+import NextRole from "@/components/NextRole";
 
 export default function Home() {
   return (
     <div className="">
          
         <Banner/>
+        <FeaturesJob/>
+        <Pricing/>
+        <NextRole/>
         
     </div>
   );
