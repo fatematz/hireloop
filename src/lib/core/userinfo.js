fd52dbdd174460f63f8be7d9bf0ba0ddfe752a83
Fatema <fatematz486@gmail.com>
2026-06-13 23:31:43 +0600

    Update userinfo.js

--- src/lib/core/userinfo.js
+++ src/lib/core/userinfo.js
@@ -1,12 +1,10 @@
-import { auth } from "@/lib/auth"; // আপনার প্রোজেক্ট অনুযায়ী auth ফাইলের সঠিক পাথ দেবেন
+import { auth } from "@/lib/auth"; 
 import { headers } from "next/headers";
 
-// ইমেজের মতো গ্লোবাল সার্ভার ফাংশন
 export const userinfo = async () => {
-    const session = await auth.api.getSession({
-        headers: await headers() // কুকিজ এবং হেডার চেক করার জন্য
+    const session = await auth.api.getSession({ //-এর কাজ: এটি সেই Cookie দিয়ে ডেটাবেজে চেক করে দেখে ইউজারটি আসলে কে, এবং তার প্রোফাইল ডেটা এনে দেয়।
+        headers: await headers() //এটি ব্রাউজার থেকে ইউজারের গোপন লগইন (Cookie) খুঁজে সার্ভারে নিয়ে আসে।
     });
 
-    // ইউজার লগইন থাকলে তার পুরো অবজেক্ট (name, email, image) রিটার্ন করবে, না থাকলে null
     return session?.user || null; 
 };
