e88390a952d88aef2fbdeeadc2dc56f8e6c3fde2
Fatema <fatematz486@gmail.com>
2026-06-16 00:57:43 +0600

    Update userinfo.js

--- src/lib/core/userinfo.js
+++ src/lib/core/userinfo.js
@@ -2,8 +2,8 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 export const userinfo = async () => {
-    const session = await auth.api.getSession({ //-এর কাজ: এটি সেই Cookie দিয়ে ডেটাবেজে চেক করে দেখে ইউজারটি আসলে কে, এবং তার প্রোফাইল ডেটা এনে দেয়।
-        headers: await headers() //এটি ব্রাউজার থেকে ইউজারের গোপন লগইন (Cookie) খুঁজে সার্ভারে নিয়ে আসে।
+    const session = await auth.api.getSession({ 
+        headers: await headers() 
     });
 
     return session?.user || null; 
